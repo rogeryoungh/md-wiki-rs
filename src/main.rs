4760a0da5bc2828bada059184a292903839898d2
rogeryoungh <rogeryoungh@foxmail.com>
2024-11-30 13:39:57 +0800

    feat: use lol_html to resolve links

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,34 @@
+use lol_html::html_content::Element;
+use lol_html::{element, HtmlRewriter, Settings};
 use minijinja::{context, path_loader};
-use pulldown_cmark::{html, Event, Options, Parser, Tag};
+use pulldown_cmark::{html, Options, Parser};
 use std::fs::{self};
 use std::path::{Path, PathBuf};
 
 fn render_md(md: &str, path: &Path) -> String {
     let parser = Parser::new_ext(md, Options::all());
 
-    let parser = parser.map(|event| match event {
-        Event::Start(Tag::Link {
-            link_type,
-            dest_url,
-            title,
-            id,
-        }) => {
-            if let Ok(url) = resolve_links(&dest_url, path) {
-                Event::Start(Tag::Link {
-                    link_type,
-                    dest_url: url.into(),
-                    title,
-                    id,
-                })
-            } else {
-                eprintln!("Failed to resolve link: {}", dest_url);
-                Event::Start(Tag::Link {
-                    link_type,
-                    dest_url,
-                    title,
-                    id,
-                })
-            }
-        }
-        _ => event,
-    });
-
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
     // html_output
-    html_output
+    html_post_proces(&html_output, path)
+}
+
+fn html_post_proces(html: &str, md_path: &Path) -> String {
+    let mut output = Vec::new();
+    let mut rewriter = HtmlRewriter::new(
+        Settings {
+            element_content_handlers: vec![element!("a[href]", |el| {
+                resolve_links(el, md_path).unwrap();
+                Ok(())
+            })],
+            ..Settings::new()
+        },
+        |c: &[u8]| output.extend_from_slice(c),
+    );
+    rewriter.write(html.as_bytes()).unwrap();
+
+    return String::from_utf8(output).unwrap();
 }
 
 fn split_query(input: &str) -> (&str, Option<&str>) {
@@ -48,11 +40,20 @@ fn split_query(input: &str) -> (&str, Option<&str>) {
     }
 }
 
-fn resolve_links(url: &str, md_path: &Path) -> Result<String, Box<dyn std::error::Error>> {
-    if url.contains("://") {
-        return Ok(url.to_owned());
+fn resolve_links(el: &mut Element, md_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let url = el.get_attribute("href").expect("href was required");
+
+    if url.contains("://") || url.starts_with('/') {
+        el.set_attribute("target", "_blank").unwrap();
+        return Ok(());
     }
-    let (path, query) = split_query(url);
+    let url = urlencoding::decode(&url).unwrap().into_owned();
+    let (path, query) = split_query(&url);
+    let path = if path.starts_with("./") {
+        &path[2..]
+    } else {
+        &path
+    };
     let new_path = md_path.parent().unwrap().join(path);
     if !new_path.with_extension("md").exists() {
         return Err(format!("File not found: {}", new_path.display()).into());
@@ -68,7 +69,9 @@ fn resolve_links(url: &str, md_path: &Path) -> Result<String, Box<dyn std::error
     if let Some(query) = query {
         new_path.push_str(query);
     }
-    return Ok(new_path);
+
+    el.set_attribute("href", &new_path).unwrap();
+    Ok(())
 }
 
 fn dfs(path: &PathBuf, vault: &Vault) {
@@ -89,14 +92,26 @@ fn dfs(path: &PathBuf, vault: &Vault) {
         let template = env.get_template("page.html").unwrap();
         let html_output: String = template
             .render(context! {
-                title => "Test",
+                title => path.file_stem().unwrap().to_str().unwrap(),
                 note_html => html_output,
             })
             .unwrap();
 
         let relative_path = path.strip_prefix(&vault.source).unwrap();
         let output_path = vault.dist.join(relative_path).with_extension("html");
+
+        if let Some(parent) = output_path.parent() {
+            fs::create_dir_all(parent).expect("Unable to create directory");
+        }
+
         fs::write(output_path, html_output).expect("Unable to write the file");
+    } else {
+        let relative_path = path.strip_prefix(&vault.source).unwrap();
+        let output_path = vault.dist.join(relative_path);
+        if let Some(parent) = output_path.parent() {
+            fs::create_dir_all(parent).expect("Unable to create directory");
+        }
+        fs::copy(path, output_path).expect("Unable to copy file");
     }
 }
 
